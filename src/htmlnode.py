469275243f8bfcb6090368e2bb06bbed3f6c25fd
anas-waleed-tahir <anastahir707070@gmail.com>
2024-12-21 08:33:03 +0500

    Add LeafNode class and implement to_html method; refactor HTMLNode initialization

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,17 +2,35 @@ from typing import List, Dict
 
 
 class HTMLNode:
-    def __init__(self, tag:str=None , value:str=None, children:List['HTMLNode']=None, props:Dict[str, str]=None):
+    def __init__(
+        self,
+        tag: str = None,
+        value: str = None,
+        children: List["HTMLNode"] = None,
+        props: Dict[str, str] = None,
+    ):
         self.tag = tag
         self.value = value
         self.children = children or []
         self.props = props or {}
-    
+
     def __repr__(self):
-        return f'{self.tag}({self.value}, {self.children}, {self.props})'
+        return f"{self.tag}({self.value}, {self.children}, {self.props})"
 
     def to_html(self) -> str:
         raise NotImplementedError()
-    
+
     def props_to_html(self) -> str:
         return " ".join([f'{key}="{value}"' for key, value in self.props.items()])
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("Leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -34,3 +34,21 @@ class TestHtmlNode(unittest.TestCase):
             node.props_to_html(),
             'src="string representing source text" height="500px"',
         )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafNode_to_html(self):
+        node = LeafNode(
+            "a", "Click here.", {"href": "https://www.google.com", "target": "_blank"}
+        )
+        print(node.to_html())
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com" target="_blank">Click here.</a>',
+        )
+
+    def test_leafNode_to_html2(self):
+        node = LeafNode(
+            None, "Click here.", {"href": "https://www.google.com", "target": "_blank"}
+        )
+        self.assertEqual(node.to_html(), "Click here.")
